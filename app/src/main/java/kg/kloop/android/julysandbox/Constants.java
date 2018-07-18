d8f67f07ce13ab9dbd0c11c2c5bc6bcca2a83466
AlexWalker28 <alexwolk28@gmail.com>
2018-07-18 19:27:55 +0600

    Finish player's choises recording in main activity

---
+++ app/src/main/java/kg/kloop/android/julysandbox/Constants.java
@@ -0,0 +1,7 @@
+package kg.kloop.android.julysandbox;
+
+public class Constants {
+    public static final int WARRIOR = 1;
+    public static final int WIZARD = 2;
+    public static final int ASSASSIN = 3;
+}
--- app/src/main/java/kg/kloop/android/julysandbox/MainActivity.java
+++ app/src/main/java/kg/kloop/android/julysandbox/MainActivity.java
@@ -2,8 +2,24 @@ package kg.kloop.android.julysandbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton first_warrior_rb;
+    private RadioButton first_wizard_rb;
+    private RadioButton first_assassin_rb;
+    private RadioButton second_warrior_rb;
+    private RadioButton second_wizard_rb;
+    private RadioButton second_assassin_rb;
+    private Button playButton;
+    private RadioGroup firstPlayerRadioGroup;
+    private RadioGroup secondPlayerRadioGroup;
+    private int firstPlayer;
+    private int secondPlayer;
 
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -12,6 +28,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first_warrior_rb = findViewById(R.id.first_warrior_rb);
+        first_wizard_rb = findViewById(R.id.first_wizard_rb);
+        first_assassin_rb = findViewById(R.id.first_assassin_rb);
+        second_warrior_rb = findViewById(R.id.second_warrior_rb);
+        second_wizard_rb = findViewById(R.id.second_wizard_rb);
+        second_assassin_rb = findViewById(R.id.second_assassin_rb);
+        playButton = findViewById(R.id.play_button);
+        firstPlayerRadioGroup = findViewById(R.id.first_player_rg);
+        secondPlayerRadioGroup = findViewById(R.id.second_player_rg);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (firstPlayerRadioGroup.getCheckedRadioButtonId()) {
+                    case R.id.first_warrior_rb:
+                        firstPlayer = Constants.WARRIOR;
+                        break;
+                    case R.id.first_wizard_rb:
+                        firstPlayer = Constants.WIZARD;
+                        break;
+                    case R.id.first_assassin_rb:
+                        firstPlayer = Constants.ASSASSIN;
+                        break;
+
+                }
+
+                switch (secondPlayerRadioGroup.getCheckedRadioButtonId()) {
+                    case R.id.second_warrior_rb:
+                        secondPlayer = Constants.WARRIOR;
+                        break;
+                    case R.id.second_wizard_rb:
+                        secondPlayer = Constants.WIZARD;
+                        break;
+                    case R.id.second_assassin_rb:
+                        secondPlayer = Constants.ASSASSIN;
+                        break;
+
+                }
+
+            }
+        });
+
 
 
     }
