940f8569e91610eddd7aefb0ad7f8cb122fb1358
AlexWalker28 <alexwolk28@gmail.com>
2018-07-18 19:38:27 +0600

    Make layout for second activity

---
+++ app/src/main/java/kg/kloop/android/julysandbox/Main2Activity.java
@@ -0,0 +1,31 @@
+package kg.kloop.android.julysandbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView firstHealthTextView;
+    TextView firstArmorTextView;
+    TextView firstSpecialTextView;
+    Button firstKickButton;
+    Button firstSpecialKickButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        firstHealthTextView = findViewById(R.id.first_health_tv);
+        firstArmorTextView = findViewById(R.id.first_armor_tv);
+        firstSpecialTextView = findViewById(R.id.first_special_tv);
+        firstKickButton = findViewById(R.id.first_kick_b);
+        firstSpecialKickButton = findViewById(R.id.first_special_kick_b);
+
+
+
+    }
+}
